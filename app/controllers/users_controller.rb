2abc13cd64e41be5941a126f7a21cb21732c69d1
Elias Kirui <eliaskips1@gmail.com>
2022-10-22 01:28:27 +0300

    Complete turbo_stream crud operations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,9 +25,11 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
+        format.turbo_stream { render turbo_stream: turbo_stream.append('user_list', partial: 'users/user', locals: { user: @user}) }
         format.html { redirect_to user_url(@user), notice: "User was successfully created." }
         format.json { render :show, status: :created, location: @user }
       else
+        format.turbo_stream { render turbo_stream: turbo_stream.replace('remote_modal', partial: 'users/form_modal', locals: {user: @user}) }
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
@@ -38,9 +40,11 @@ class UsersController < ApplicationController
   def update
     respond_to do |format|
       if @user.update(user_params)
+        format.turbo_stream { render turbo_stream: turbo_stream.replace("user_row_#{@user.id}", partial: 'users/user', locals: { user: @user }) }
         format.html { redirect_to user_url(@user), notice: "User was successfully updated." }
         format.json { render :show, status: :ok, location: @user }
       else
+        format.turbo_stream { render turbo_stream: turbo_stream.replace('remote_modal', partial: 'users/form_modal', locals: { user: @user}) }
         format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
@@ -52,6 +56,7 @@ class UsersController < ApplicationController
     @user.destroy
 
     respond_to do |format|
+      format.turbo_stream { render turbo_stream: turbo_stream.remove("user_row_#{@user.id}") }
       format.html { redirect_to users_url, notice: "User was successfully destroyed." }
       format.json { head :no_content }
     end
@@ -65,6 +70,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:name, :email, :city, :state, :contact_number)
+      params.require(:user).permit(:name, :email, :city, :state, :country, :contact_number)
     end
 end
